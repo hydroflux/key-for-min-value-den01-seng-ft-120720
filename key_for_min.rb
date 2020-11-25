25416dcb36ed40743de357de52f6ed58ea30c3a9
Jack Hubert <jphubert93@gmail.com>
2020-11-25 01:32:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_key = ''
+  min_value = 1000
+  
+  if name_hash.length == 0
+    return nil
+  end
+  
+  name_hash.each do |key, value|
+    if value < min_value
+      min_value = value
+      min_key = key
+    end
+  end
+  min_key
 end
